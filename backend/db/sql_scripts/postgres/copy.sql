81d03a837574ee15e4164971a6d1727dea5561e6
nisu <nisu@debian.myguest.virtualbox.org>
2025-09-10 22:59:29 +0300

    refactor sql scripts

--- backend/db/sql_scripts/postgres/copy.sql
+++ backend/db/sql_scripts/postgres/copy.sql
@@ -5,3 +5,6 @@ COPY CarSnapshot FROM '/data/snaps.csv' DELIMITER ',' CSV HEADER;
 COPY STS FROM '/data/stss.csv' DELIMITER ',' CSV HEADER;
 COPY PTS FROM '/data/ptss.csv' DELIMITER ',' CSV HEADER;
 COPY AppUser FROM '/data/users.csv' DELIMITER ',' CSV HEADER;
+
+SELECT setval('carsnapshot_id_seq', (SELECT COALESCE(MAX(id), 0) + 1 FROM carsnapshot));
+SELECT setval('appuser_id_seq', (SELECT COALESCE(MAX(id), 0) + 1 FROM appuser));
